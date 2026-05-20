b00b7f5bca84284380552b4c9cc254225d78202b
lumicore-dev <lumicore-dev@users.noreply.github.com>
2026-05-20 12:42:58 +0000

    deploy: 7b0bac37f3c70b024773982aba6c8f5544200cd1

--- en/searcher-09f2665d.js
+++ en/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0465bfe5.js',
+                path_to_root + 'searchindex-21a29cfd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
