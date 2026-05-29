cd1e56752106ff9ce65d463ec04e83e4a5a306c7
lumicore-dev <lumicore-dev@users.noreply.github.com>
2026-05-29 02:09:31 +0000

    deploy: dbff7dd6ee50db1da9f5f8cf29307d8e92409cb9

--- zh/searcher-09f2665d.js
+++ zh/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e1a56556.js',
+                path_to_root + 'searchindex-ac871418.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
